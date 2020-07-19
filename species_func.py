9c2c7cbc07d2ca12b8c6c70b3209258f5a33c48e
Eon Viljoen <eonviljoen.ev@gmail.com>
2020-07-19 11:21:40 +0200

    Prototype api and json with interface

---
+++ species_func.py
@@ -0,0 +1,29 @@
+from flask import jsonify, request
+import json
+
+
+# Read JSON
+with open('species.json', 'r') as myfile:
+    information_file=myfile.read()
+
+# Load JSON
+species = json.loads(information_file) 
+
+# Return item if id exists
+def return_item_with_id(id):
+    for item in species['list']:
+        if item['id'] == id:
+            return (item)
+
+
+
+# print(species['list'][0]['name'])
+
+# Return list of ids
+def get_species_id_list():
+    id_results = []
+
+    for thing in species['list']:
+        id_results.append(thing['id'])
+
+    return (id_results)
